32fe91d76a703ae174822cc14639a930e17a21d7
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-09-16 13:59:13 -0300

    fix: apaguei arquivos e informações desnecessárias

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import User from './nomeComMapjs';
+import { joao, users } from './data/dados';
+import NomeSemMap from './nomeSemMap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+
+    return (
+      <div>
+        <NomeSemMap dados={ joao } />
+        {users.map((user) => <User user={ user } />)}
+      </div>
+    );
+  }
 }
 
 export default App;
