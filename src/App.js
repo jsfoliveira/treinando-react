04995f4f6d5d3f451352d89b9ac6039e73fa77c2
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-09-19 20:21:40 -0300

    build: configurando o router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
 import User from './componentes/NomeComMap';
-import { joao, users } from './data/dados';
+import { joao,  users } from './data/dados';
 import NomeSemMap from './componentes/NomeSemMap';
 import Pokedex from './componentes/Pokedex';
 import { Pokemon } from './componentes/Pokemon';
 import pokemon from './data/pokemon';
+import { Form } from './componentes/Form';
 
 class App extends React.Component {
   render() {
 
     return (
-      <div>
-        <NomeSemMap dados={ joao } />
-        {users.map((user) => <User user={ user } />)}
-        <Pokedex />
-        {pokemon.map((pokemon) => <Pokemon pokemon={ pokemon } />)}
-      </div>
+      <BrowserRouter>
+       <Switch>
+        <Route exact path="/form" component={ Form }/>
+        <Route exact path="/joao" render={ () => <NomeSemMap dados={ joao } /> } />
+        <Route exact path="/pokedex" component={ Pokedex }/>
+        <Route exact path="/user" render={ () => users.map((user) => <User user={ user }/>)} />
+        <Route exact path="/pokemon" render={ () => pokemon.map((pokemon) => <Pokemon pokemon={ pokemon } />)} />
+       </Switch>
+      </BrowserRouter>
+
     );
   }
 }
